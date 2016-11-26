bda28a36698bf8fcde80c61cb46f9c9cc440c2e9
Bus000 <magnus@stavngaard.dk>
2016-11-26 16:06:42 +0100

    Add main method

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,23 @@
 module Main where
 
+import System.Environment
+    ( getArgs
+    )
+
+import Parser.Impl
+    ( parseString
+    )
+
+import Interpreter.Impl
+    ( interpret
+    )
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    args <- getArgs
+
+    case args of
+        [str] -> case parseString str of
+            Right program -> print $ interpret program
+            Left error -> putStrLn error
+        _ -> putStrLn "Expect a single argument"
