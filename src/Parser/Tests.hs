5f0d2af0e84bb21f2e3fa167de2323c59de4eff5
Bus000 <magnus@stavngaard.dk>
2016-11-28 12:34:48 +0100

    Cleanup

--- src/Parser/Tests.hs
+++ src/Parser/Tests.hs
@@ -46,13 +46,13 @@ unitTests = testGroup "Unit tests"
     ]
 
 literal1 :: Assertion
-literal1 = [(Literal $ Whole 10, "")] @=? readP_to_S parseExpr2 "10"
+literal1 = [(ten, "")] @=? readP_to_S parseExpr2 "10"
 
 literal2 :: Assertion
-literal2 = [(Literal $ Real 10, "")] @=? readP_to_S parseExpr2 "10.0"
+literal2 = [(ten, "")] @=? readP_to_S parseExpr2 "10.0"
 
 literal3 :: Assertion
-literal3 = [(Literal $ Whole 10, "  ")] @=? readP_to_S parseExpr2 "  10  "
+literal3 = [(ten, "  ")] @=? readP_to_S parseExpr2 "  10  "
 
 literal4 :: Assertion
 literal4 = [(Literal $ Real 124.001, "")] @=? readP_to_S parseExpr2 "  124.001"
@@ -60,43 +60,41 @@ literal4 = [(Literal $ Real 124.001, "")] @=? readP_to_S parseExpr2 "  124.001"
 product1 :: Assertion
 product1 = assertBool "" $ result `elem` readP_to_S parseExpr1 program
   where
-    result = (Product (Literal $ Whole 10) (Literal $ Whole 20), "")
+    result = (Product ten (Literal $ Whole 20), "")
     program = "10 * 20"
 
 product2 :: Assertion
 product2 = assertBool "" $ result `elem` readP_to_S parseExpr1 program
   where
-    result = (Product (Product (Product (Product
-        (Literal $ Whole 1) (Literal $ Whole 2)) (Literal $ Whole 3))
-        (Literal $ Whole 4)) (Literal $ Whole 5), " ")
-    program = " 1 * 2 * 3 * 4 * 5 "
+    result = (Product (Product (Product (Product one two) three) four) five, "")
+    program = " 1 * 2 * 3 * 4 * 5"
 
 product3 :: Assertion
 product3 = assertBool "" $ result `elem` readP_to_S parseExpr1 program
   where
-    result = (Product (Literal $ Whole 10) (Literal $ Whole 20), "")
+    result = (Product ten (Literal $ Whole 20), "")
     program = "10 \\cdot 20"
 
 product4 :: Assertion
 product4 = assertBool "" $ result `elem` readP_to_S parseExpr1 program
   where
-    result = (Product (Literal $ Whole 10) (Literal $ Whole 20), "")
+    result = (Product ten (Literal $ Whole 20), "")
     program = "10 \\times 20"
 
 fraction1 :: Assertion
 fraction1 = assertBool "" $ result `elem` readP_to_S parseExpr2 program
   where
-    result = (Fraction (Literal $ Whole 10) (Literal $ Whole 20), "")
+    result = (Fraction ten (Literal $ Whole 20), "")
     program = "\\frac{10}{20}"
 
 fraction2 :: Assertion
 fraction2 = assertBool "" $ result `elem` readP_to_S parseExpr2 program
   where
-    result = (Fraction (Literal $ Whole 10) (Literal $ Whole 20), "")
+    result = (Fraction ten (Literal $ Whole 20), "")
     program = "\\frac  { 10\t}   {  20 }  "
 
 factorial1 :: Assertion
-factorial1 = assertBool ""$  result `elem` readP_to_S parseExpr2 program
+factorial1 = assertBool "" $ result `elem` readP_to_S parseExpr2 program
   where
     result = (Factorial (Literal $ Whole 5), "")
     program = "5!"
@@ -104,14 +102,13 @@ factorial1 = assertBool ""$  result `elem` readP_to_S parseExpr2 program
 factorial2 :: Assertion
 factorial2 = result @=? readP_to_S parseExpr program
   where
-    result = [(Sum (Literal $ Whole 5) (Factorial (Literal $ Whole 6)), "")]
+    result = [(Sum five (Factorial six), "")]
     program = "5 + 6!"
 
 expr1 :: Assertion
 expr1 = Right result @=? parseString program
   where
-    result = Sum (Sum (Literal $ Whole 2) (Product (Literal $ Whole 3)
-        (Literal $ Whole 4))) (Literal $ Whole 5)
+    result = Sum (Sum two (Product three four)) five
     program = " 2 + 3   * 4  +  5   "
 
 expr2 :: Assertion
@@ -121,55 +118,51 @@ expr2 = Right result @=? parseString program
     over = fact10
     under = Product fact10
         (Factorial (Minus (Literal $ Whole 10) (Literal $ Whole 5)))
-    fact10 = Factorial (Literal $ Whole 10)
+    fact10 = Factorial ten
     program = "\\frac{10!}{10! \\cdot (10 - 5)!}"
 
 expr3 :: Assertion
 expr3 = Right result @=? parseString program
   where
-    result = Product (Literal $ Whole 2)
-        (Sum (Literal $ Whole 5) (Literal $ Whole 4))
+    result = Product two (Sum five four)
     program = "2 * (5 + 4)"
 
 expr4 :: Assertion
 expr4 = Right result @=? parseString program
   where
-    result = Sum (Literal $ Whole 5) (Literal $ Whole 10)
+    result = Sum five ten
     program = "(5 + 10)"
 
 expr5 :: Assertion
 expr5 = Right result @=? parseString program
   where
-    result = Literal $ Whole 5
+    result = five
     program = "(5)"
 
 expr6 :: Assertion
 expr6 = Right result @=? parseString program
   where
-    result = Factorial (Product (Literal $ Whole 2)
-        (Sum (Literal $ Whole 5) (Literal $ Whole 4)))
+    result = Factorial (Product two (Sum five four))
     program = "(2 * \\left(5 + 4\\right))!"
 
 expr7 :: Assertion
 expr7 = Right result @=? parseString program
   where
     result = Fraction over under
-    over = Minus (Literal $ Whole 1)
-        (Fraction (Literal $ Whole 1) (Literal $ Whole 2))
+    over = Minus one (Fraction one two)
     under = Literal $ Whole 0
     program = "\\frac{1 - \\left( \\frac{1}{2} \\right)}{0}"
 
 expr8 :: Assertion
 expr8 = Right result @=? parseString program
   where
-    result = Fraction (Literal $ Whole 1) (Literal $ Whole 2)
+    result = Fraction one two
     program = "\\left( \\frac{1}{2} \\right)"
 
 expr9 :: Assertion
 expr9 = Right result @=? parseString program
   where
-    result = Minus (Literal $ Whole 1)
-        (Fraction (Literal $ Whole 1) (Literal $ Whole 2))
+    result = Minus one (Fraction one two)
     program = "1 - \\left( \\frac{1}{2} \\right)\n"
 
 expr10 :: Assertion
